9281c535fb586229499304e0401031964d584f2f
gabrielsouss77 <gs9245111@gmail.com>
2025-03-27 16:15:49 -0300

    primeiro

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static  void main(String[] args){
+        calculos();
+    }
+    static void calculos (){
+        int n1 = 123;
+        int n2 = 10;
+        int n3 = 543;
+        System.out.println(n1+n2);
+        System.out.println(n3 / n1);
+    }
+
+
+}
